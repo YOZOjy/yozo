4bf2f8a752fe574f86e9388e9b2ad9f4cb03bf88
YOZOjy <89232931+YOZOjy@users.noreply.github.com>
2021-08-30 10:34:35 +0800

    Add files via upload

---
+++ yozo_api/src/index.d.ts
@@ -0,0 +1,26 @@
+/// <reference path="office.d.ts" />
+/// <reference path="wp.d.ts" />
+/// <reference path="ss.d.ts" />
+/// <reference path="ppt.d.ts" />
+/// <reference path="vb.d.ts" />
+
+declare namespace yozo {
+  export function WApplication(): word.Application
+  export function SApplication(): excel.Application
+  export function PApplication(): powerpoint.Application
+
+  export function showDialog(url: string, title?: string, width?: number, height?: number, isModal?: boolean): void;
+  export function showTaskPane(plugin: any, taskName: string);
+  export function setControlState(controlId: String , controlState: int): void;
+
+  export interface Plugin {
+    readonly descriptor: PluginDescriptor
+  }
+  export interface PluginDescriptor {
+    readonly name: string
+    readonly type: string
+    readonly version: string
+    readonly location: string
+    readonly localLocation: string
+  }
+}
